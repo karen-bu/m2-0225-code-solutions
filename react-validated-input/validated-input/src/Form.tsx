84d37654cbd8889e8dbf7b9ce2a8ec58c23df0c1
Karen Bu <ktianbu@gmail.com>
2025-07-19 20:07:07 +0000

    Finished exercise react-validated-input

---
+++ react-validated-input/validated-input/src/Form.tsx
@@ -0,0 +1,115 @@
+import './Form.css';
+import { useState } from 'react';
+
+export function ValidatedInput() {
+  const [password, setPassword] = useState('');
+
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const caps = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
+  function numCheck(password: string) {
+    for (let i = 0; i < numbers.length; i++) {
+      if (password.includes(String(numbers[i]))) {
+        return true;
+      }
+    }
+  }
+
+  function capsCheck(password: string) {
+    for (let i = 0; i < caps.length; i++) {
+      if (password.includes(caps[i])) {
+        return true;
+      }
+    }
+  }
+
+  function specCharCheck(password: string) {
+    for (let i = 0; i < specialCharacters.length; i++) {
+      if (password.includes(specialCharacters[i])) {
+        return true;
+      }
+    }
+  }
+
+  function displayErrorMessage(password: string) {
+    if (password === '') {
+      return '';
+    } else if (password.length < 8) {
+      return 'your password must be more than 8 characters in length';
+    } else if (!numCheck(password)) {
+      return 'your password must contain a number';
+    } else if (!capsCheck(password)) {
+      return 'your password must contain a capital letter';
+    } else if (!specCharCheck(password)) {
+      return 'your password must contain a special character - !, @, #, %, ^, &, *, (, or )';
+    }
+  }
+
+  function displayErrorIcon(password: string) {
+    if (password === '') {
+      return '';
+    } else if (password.length < 8) {
+      return '❌';
+    } else if (!numCheck(password)) {
+      return '❌';
+    } else if (!capsCheck(password)) {
+      return '❌';
+    } else if (!specCharCheck(password)) {
+      return '❌';
+    }
+  }
+
+  const errorMessage = displayErrorMessage(password);
+  const errorIcon = displayErrorIcon(password);
+
+  return (
+    <div>
+      <div>
+        <h3>Password</h3>
+      </div>
+      <div className="input-holder">
+        <div className="column-80">
+          <input
+            type="text"
+            placeholder="please enter your password here!"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}></input>
+        </div>
+        <div className="column-20">
+          <div>{errorIcon}</div>
+        </div>
+      </div>
+      <div>
+        <p>{errorMessage}</p>
+      </div>
+    </div>
+  );
+}
